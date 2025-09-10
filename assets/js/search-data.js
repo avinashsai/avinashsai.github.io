db265d94c27474321123dd2f34d889d3244dccaa
avinashsai <avinashmadasu17@gmail.com>
2025-09-10 21:13:54 +0000

    Deploying to gh-pages from @ avinashsai/avinashsai.github.io@cce9a4cce2e3917edbd5533878176afb8b6d42b1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
         },{id: "nav-repositories",
           title: "repositories",
           description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
