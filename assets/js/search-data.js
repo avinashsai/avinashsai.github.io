374510aa14a83f404799f94c7c8cd9ad4c23417f
avinashsai <avinashmadasu17@gmail.com>
2025-09-10 21:40:01 +0000

    Deploying to gh-pages from @ avinashsai/avinashsai.github.io@8ed71af11674a5459773cf4007ade55f1a945f7e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-repositories",
           title: "Repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/repositories/";
