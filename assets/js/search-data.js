e9bdcfa954ed51de38b3229e29586262930ca21b
avinashsai <avinashmadasu17@gmail.com>
2025-09-14 02:55:02 +0000

    Deploying to gh-pages from @ avinashsai/avinashsai.github.io@56ef5d8e23809bd27ca77e90762222e6c75a0e3a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,21 +30,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/books/";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/blog/";
-              },
-            },{id: "post-a-post-with-plotly-js",
+        },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
